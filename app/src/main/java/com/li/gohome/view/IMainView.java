800235e2333a06f3863101b6745166e6343e0f13
lc <693472074@qq.com>
2019-11-13 15:37:03 +0800

    tablayout+recycleview

---
+++ app/src/main/java/com/li/gohome/view/IMainView.java
@@ -0,0 +1,11 @@
+package com.li.gohome.view;
+
+import com.li.gohome.baen.HomeModel;
+import com.li.gohome.baen.TravelModel;
+import com.li.gohome.baen.TravelTabModel;
+
+public interface IMainView {
+    void getHomeModel(HomeModel homeModel);
+    void getTravelModel(TravelModel travelModel);
+    void getTravelTabModel(TravelTabModel travelTabModel);
+}
